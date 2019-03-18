fb90016ede6a74c8395aa960bb8efdb37ad88cf5
Alpha Diallo <aob.diallo@gmail.com>
2019-03-18 15:26:18 +0100

    middleware definition

---
+++ tasks_app/middleware.py
@@ -0,0 +1,8 @@
+"""Flask middleware definitions. This is also where filters are defined."""
+
+from flask import current_app, make_response, jsonify
+
+
+@current_app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
